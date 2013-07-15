046aff2f0aded166447afad9f611b6575b556660
Neil Conway <nrc@cs.berkeley.edu>
2013-07-15 13:16:19 -0700

    New simple reliable delivery program.

---
+++ broadcast/single_recv.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'bud'
+
+class SingleRecv
+  include Bud
+
+  state do
+    table :sbuf, [:id] => [:addr, :val]
+    table :rbuf, sbuf.schema
+    channel :chn, [:id] => [:@addr, :val]
+  end
+
+  bloom do
+    chn   <~ sbuf
+    rbuf  <= chn
+    stdio <~ chn.inspected
+  end
+end
+
+s = SingleRecv.new
+s.run_bg
+
+r = SingleRecv.new
+r.run_bg
+
+s.sync_do {
+  s.sbuf <+ [[1, r.ip_port, 'foo'],
+             [2, r.ip_port, 'bar']]
+}
+
+sleep 3
+
+s.stop
+r.stop
