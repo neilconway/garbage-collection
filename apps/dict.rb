24383118a56a308ee480abeb885e3f7315219430
Neil Conway <nrc@cs.berkeley.edu>
2013-07-22 13:38:46 -0700

    Checkpoint.

--- apps/dict.rb
+++ apps/dict.rb
@@ -29,12 +29,36 @@ require 'bud'
 class ReplDict
   include Bud
 
+  def initialize(addrs, opts={})
+    @addr_list = addrs
+    super(ops)
+  end
+
+  bootstrap do
+    node <= @addr_list.map {|a| [a]}
+  end
+
   state do
     table :node, [:addr]        # XXX: immutable
     table :log, [:creator, :id] => [:op_type, :key, :val]
+    channel :chn, [:creator, :id] => [:op_type, :key, :val, :@addr]
     scratch :view, [:key] => [:val]
+    scratch :ins_ops, [:key] => [:val]
+    scratch :del_ops, [:key]
   end
 
   bloom do
+    ins_ops <= log {|l| [l.key, l.val] if l.op_type == INSERT_OP}
+    del_ops <= log {|l| [l.key] if l.op_type == DELETE_OP}
+    view <= ins_ops.notin(del_ops, :key => :key)
+
+    chn <~ (log * node).pairs {|l,n| l + [n.addr]}
+    log <= chn
   end
 end
+
+ports = (1..3).map {|i| i + 10001}
+addrs = ports.map {|p| "localhost:#{p}"}
+
+rlist = ports.map {|p| ReplDict.new(addrs, :ip => "localhost", :port => p)}
+rlist.each(&:run_bg)
