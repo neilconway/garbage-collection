9fa82cc32299e6037726bc3e5fe3c30a092fd7e1
Neil Conway <nrc@cs.berkeley.edu>
2013-08-12 16:57:40 -0700

    Fix a bug in causal consistency.
    The set of good puts are those whose dependencies are also good puts; the
    previous coding checked for puts whose (direct) dependencies have been observed,
    but that doesn't account for the case where we know about the dependencies of an
    op but some of the dependencies-of-the-dependencies are missing.

--- causalkvs/causal-core.rb
+++ causalkvs/causal-core.rb
@@ -7,28 +7,23 @@ module CausalCore
     # assumption: reqids are globally unique and externally generated.  
     # no ordering or other semantic assumptions.  Hence we can represent dependencies
     # as key, reqid pairs (but can't use integer comparison for dominance)
-    table :put_log, [:reqid, :key, :value, :deps]
+    table :put_log, [:reqid] => [:key, :value, :deps]
     scratch :active_puts, put_log.schema
 
-    # helper collections
-    scratch :flat_deps, [:reqid, :key, :depid]
-    scratch :good_put, put_log.schema
+    scratch :flat_deps, [:reqid, :depid]
     scratch :missing_deps, flat_deps.schema
+    table :good_put, put_log.schema
     table :dominated, [:reqid]
     scratch :contains, [:r1, :r2]
   end
 
-  bloom do
-    stdio <~ active_puts{|p| ["ACTIVE(#{budtime}): #{p}"]}
-  end
-
   bloom :satisfied do 
     flat_deps <= put_log.flat_map do |p|
-      p.deps.map{|d| [p.reqid, p.key, d]}
+      p.deps.map{|d| [p.reqid, d]}
     end
 
-    missing_deps <= flat_deps.notin(put_log, :depid => :reqid)
-    good_put <= put_log.notin(missing_deps, :reqid => :reqid)
+    missing_deps <= flat_deps.notin(good_put, :depid => :reqid)
+    good_put <+ put_log.notin(missing_deps, :reqid => :reqid)
     active_puts <= (put_log * good_put).lefts(:reqid => :reqid).notin(dominated, :reqid => :reqid)
   end
 
@@ -51,16 +46,18 @@ c = CC.new
 c.put_log <+ [[1, "foo", "bar", []],
               [2, "foo", "baz", [1]],
               [3, "foo", "qux", [1,4]]]
+c.tick; c.tick; c.tick
 
-c.tick
-
-puts "GOOD PUTS: #{c.good_put.to_a.sort.inspect}"
+puts c.active_puts.to_a.sort.inspect
 
 c.put_log <+ [[4, "bar", "eek", []]]
+c.tick; c.tick; c.tick
 
-c.tick
+puts c.active_puts.to_a.sort.inspect
 
-c.put_log.each{|p| puts "PL: #{p}"}
+c.put_log <+ [[5, "foo", "quux", [1, 4, 8]],
+              [6, "trick", "treat", [1, 5]],
+              [7, "k", "v", [2]]]
+c.tick; c.tick
 
-c.put_log <+ [[5, "foo", "quux", [1, 4, 7]]]
-c.tick
+puts c.active_puts.to_a.sort.inspect
