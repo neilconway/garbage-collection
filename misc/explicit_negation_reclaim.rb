4d96f95e13214fe70e22dcfa4b1294138f14dacd
Neil Conway <nrc@cs.berkeley.edu>
2013-08-13 16:49:07 -0700

    Add sealing metadata for GC reclaim example

--- misc/explicit_negation_reclaim.rb
+++ misc/explicit_negation_reclaim.rb
@@ -13,6 +13,9 @@ class NegationTest
     scratch :got_ack, [:id, :msg_epoch, :addr, :addr_epoch]
     scratch :missing_val, got_ack.schema
     scratch :to_reclaim, sbuf.schema
+
+    # Sealing metadata
+    table :node_seal_epoch, [:epoch]
   end
 
   bloom do
@@ -26,8 +29,9 @@ class NegationTest
     # epoch cannot (yet) be reclaimed
     missing_val <= ((sbuf * node).pairs(:epoch => :epoch) {|s,n| s + n}).notin(got_ack)
 
-    # We can reclaim whatever isn't unsafe to reclaim
-    to_reclaim <= sbuf.notin(missing_val, :id => :id, :epoch => :msg_epoch)
+    # We can reclaim whatever isn't unsafe to reclaim, provided the epoch to
+    # which the message belongs has been sealed
+    to_reclaim <= (sbuf * node_seal_epoch).lefts(:epoch => :epoch).notin(missing_val, :id => :id, :epoch => :msg_epoch)
   end
 end
 
@@ -45,7 +49,19 @@ n.tick
 puts "MISSING: #{n.missing_val.to_a.inspect}"
 puts "TO RECLAIM: #{n.to_reclaim.to_a.inspect}"
 
+n.node_seal_epoch <+ [[1]]
+n.tick
+
+puts "MISSING: #{n.missing_val.to_a.inspect}"
+puts "TO RECLAIM: #{n.to_reclaim.to_a.inspect}"
+
 n.send_ack <+ [[7, "baz"]]
+n.tick
+
+puts "MISSING: #{n.missing_val.to_a.inspect}"
+puts "TO RECLAIM: #{n.to_reclaim.to_a.inspect}"
+
+n.node_seal_epoch <+ [[2]]
 n.tick
 
 puts "MISSING: #{n.missing_val.to_a.inspect}"
