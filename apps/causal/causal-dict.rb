28d5530fdbbacfe0893ecf5ba5b9f58578af4e26
Neil Conway <nrc@cs.berkeley.edu>
2013-08-22 11:34:48 -0700

    Add to causal dictionary example.

--- apps/causal/causal-dict.rb
+++ apps/causal/causal-dict.rb
@@ -40,6 +40,30 @@ end
 
 ports = (1..3).map {|i| i + 10001}
 addrs = ports.map {|p| "127.0.0.1:#{p}"}
-rlist = ports.map {|p| CausalDict.new(addrs, :ip => "localhost", :port => :p)}
+rlist = ports.map {|p| CausalDict.new(:ip => "localhost", :port => p)}
+rlist.each do |r|
+  r.node <+ addrs.map {|a| [a]}
+  r.run_bg
+end
 
-d = CausalDict.new
+first = rlist.first
+first.sync_do {
+  first.log <+ [[[first.port, 1], 'foo', 'bar', []]]
+}
+
+last = rlist.last
+last.sync_do {
+  last.log <+ [[[last.port, 1], 'baz', 'qux', [[first.port, 1]]]]
+}
+
+last.sync_do {
+  last.log <+ [[[last.port, 2], 'baz', 'kkk', []]]
+}
+
+sleep 1
+
+first.sync_do {
+  puts first.view.map {|v| "#{v.key} => #{v.val}"}.sort.inspect
+}
+
+rlist.each(&:stop)
