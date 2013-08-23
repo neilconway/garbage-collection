120c1ce9636ca00702d6256415ac78bd264a6277
Neil Conway <nrc@cs.berkeley.edu>
2013-08-23 13:06:57 -0700

    Improvements to causal dict program.
    RCE + RSE works for read_req => req_chn.
    
    RCE works for log => chn, read_resp => resp_chn.

--- apps/causal/causal-dict.rb
+++ apps/causal/causal-dict.rb
@@ -1,27 +1,6 @@
 require 'rubygems'
 require 'bud'
 
-module ReadProtocol
-  state do
-    channel :req_chn, [:@addr, :id] => [:key, :deps]
-    channel :resp_chn, [:@addr, :id, :key, :val]
-  end
-end
-
-class CausalClient
-  include Bud
-  include ReadProtocol
-
-  state do
-    table :read_req, req_chn.schema
-  end
-
-  bloom do
-    req_chn <~ read_req
-    stdio <~ resp_chn {|r| ["CLIENT: #{r.key} => #{r.val} (#{r.id})"]}
-  end
-end
-
 # Places where we would like to apply RCE:
 #   (1) read_req => read_chn
 #   (2) (node * log) => chn
@@ -32,21 +11,25 @@ end
 #   (2) (node * log).notin(chn_approx) (post-RCE)
 #   (3) read_resp.notin(resp_chn_approx) (post-RCE)
 #   (4) safe_log.notin(dominated)
-#   (5) read_buf.notin(read_response)
+#   (5) read_buf.notin(read_resp)
 #
 # Places where we would like to apply notin compression:
 #   (1) read_chn_approx (post-RCE)
 #   (2) chn_approx (post-RCE)
 #   (3) resp_chn_approx (post-RCE)
-#   (4) read_response
+#   (4) read_resp
 #       => This is a little tricky: the intuition is that once a response has
 #          been generated and sent to the client, we only need to keep around
 #          enough information so that the negation against read_buf can be
 #          performed. So this is like a combination of normal notin compression
 #          and RSE.
+#
+# XXX: It would make sense to split the client-side code into a separate class,
+# and move the read channel state into a shared module. However, the current
+# analysis is per-class, so this would prevent doing RCE/RSE on the read
+# protocol.
 class CausalDict
   include Bud
-  include ReadProtocol
 
   state do
     sealed :node, [:addr]
@@ -59,12 +42,20 @@ class CausalDict
     scratch :dominated, log.schema
     scratch :view, log.schema
 
+    # State for handling read requests
+    channel :req_chn, [:@addr, :id] => [:key, :deps]
+    channel :resp_chn, [:@addr, :id, :key, :val]
+
+    # Client-side read state
+    table :read_req, req_chn.schema
+
+    # Server-side read state
     table :read_buf, [:id] => [:key, :deps, :src_addr]
     scratch :read_pending, read_buf.schema
     scratch :read_dep, [:id, :dep]
     scratch :missing_read_dep, read_dep.schema
     scratch :safe_read, read_buf.schema
-    table :read_response, resp_chn.schema
+    table :read_resp, resp_chn.schema
   end
 
   bloom :replication do
@@ -85,27 +76,33 @@ class CausalDict
     view <= safe_log.notin(dominated)
   end
 
-  bloom :read_path do
+  bloom :read_server do
     # XXX: Intuitively there should be a cleaner way to write this. We'd like to
-    # say that "read_pending is the delta between read_response and read_buf".
+    # say that "read_pending is the delta between read_resp and read_buf".
     read_buf <= req_chn {|r| [r.id, r.key, r.deps, r.source_addr]}
-    read_pending <= read_buf.notin(read_response, :id => :id)
+    read_pending <= read_buf.notin(read_resp, :id => :id)
     read_dep <= read_pending.flat_map {|r| r.deps.map {|d| [r.id, d]}}
     missing_read_dep <= read_dep.notin(safe_log, :dep => :id)
     safe_read <= read_buf.notin(missing_read_dep, :id => :id)
-    read_response <+ (safe_read * view).pairs(:key => :key) {|r,v| [r.src_addr, r.id, r.key, v.val]}
-    resp_chn <~ read_response
+    read_resp <+ (safe_read * view).pairs(:key => :key) {|r,v| [r.src_addr, r.id, r.key, v.val]}
+    resp_chn <~ read_resp
+  end
+
+  bloom :read_client do
+    req_chn <~ read_req
+    stdio <~ resp_chn {|r| ["CLIENT: #{r.key} => #{r.val} (#{r.id})"]}
   end
 
   def print_view
     puts "View @ #{port}:"
-    puts view.map {|v| "\t#{v.key} => #{v.val}"}.join("\n")
+    puts view.map {|v| "\t#{v.key} => #{v.val}"}.sort.join("\n")
   end
 end
 
+opts = { :channel_stats => true }
 ports = (1..3).map {|i| i + 10001}
 addrs = ports.map {|p| "localhost:#{p}"}
-rlist = ports.map {|p| CausalDict.new(:ip => "localhost", :port => p, :channel_stats => true)}
+rlist = ports.map {|p| CausalDict.new(opts.merge(:ip => "localhost", :port => p))}
 rlist.each do |r|
   r.node <+ addrs.map {|a| [a]}
   r.tick
@@ -119,7 +116,7 @@ last.log <+ [[[last.port, 1], 'baz', 'qux', [[first.port, 1]]]]
 last.log <+ [[[last.port, 2], 'baz', 'kkk', []],
              [[last.port, 3], 'baz', 'kkk2', [[first.port, 2]]]]
 
-c = CausalClient.new(:channel_stats => true)
+c = CausalDict.new(opts)
 c.tick
 c.read_req <+ [[last.ip_port, [c.port, 1], 'foo', [[first.port, 1]]]]
 c.tick
@@ -128,6 +125,12 @@ c.tick
 
 first.print_view
 last.print_view
+
+puts "# of stored client-side requests: #{c.read_req.to_a.size}"
+puts "# of buffered read requests @ server: #{last.read_buf.to_a.size}"
+puts "# of stored read responses @ server: #{last.read_resp.to_a.size}"
+puts "# of log entries @ server: #{last.log.to_a.size}"
+puts "# of safe log entries @ server: #{last.safe_log.to_a.size}"
 
 c.stop
 rlist.each(&:stop)
