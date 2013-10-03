c628c3095e3f268570c278173f76a112d0a3d153
Neil Conway <nrc@cs.berkeley.edu>
2013-10-02 22:39:15 -0700

    Initial version of req-response program.

---
+++ apps/read-req.rb
@@ -0,0 +1,64 @@
+require 'rubygems'
+require 'bud'
+
+class RequestResponder
+  include Bud
+
+  state do
+    channel :req_chn, [:@addr, :client, :id, :key]
+    channel :resp_chn, [:@addr, :id, :key] => [:val]
+
+    table :req_log, [:client, :id, :key]
+    table :resp_log, [:client, :id, :key, :val]
+    table :did_resp, [:id]
+
+    scratch :need_resp, req_log.schema
+
+    table :state
+
+    # Client-side state
+    table :read_req, req_chn.schema
+    table :read_resp, resp_chn.schema
+  end
+
+  bloom do
+    req_log <= req_chn.payloads
+    resp_chn <~ resp_log
+
+    need_resp <= req_log.notin(did_resp, :id => :id)
+    # TODO: handle missing keys
+    resp_log <= (need_resp * state).pairs(:key => :key) do |r,s|
+      r + [s.val]
+    end
+    did_resp <= resp_log {|r| [r.id]}
+  end
+
+  bloom :client do
+    req_chn <~ read_req
+    read_resp <= resp_chn
+  end
+
+  def print_resp
+    puts "Client-side responses:"
+    puts read_resp.map {|r| "\t#{r.key} => #{r.val}"}.sort.join("\n")
+  end
+end
+
+s = RequestResponder.new(:port => 5555, :channel_stats => true)
+s.state <+ [["foo1", "bar"], ["foo2", "baz"]]
+
+c = RequestResponder.new(:port => 5556, :channel_stats => true)
+c.read_req <+ [[s.ip_port, c.ip_port, 1, "foo1"],
+               [s.ip_port, c.ip_port, 2, "foo2"]]
+
+nodes = [s, c]
+10.times { nodes.each(&:tick); sleep(0.1); }
+
+c.print_resp
+
+puts "(Server) Size of req_log: #{s.req_log.to_a.size}"
+puts "(Server) Size of resp_log: #{s.resp_log.to_a.size}"
+puts "(Server) Size of did_resp: #{s.did_resp.to_a.size}"
+puts "(Client) Size of read_req: #{c.read_req.to_a.size}"
+
+nodes.each(&:stop)
