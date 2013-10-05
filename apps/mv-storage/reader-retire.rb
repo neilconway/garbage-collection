e01fbfc38189587876341b2589fe73b31dbeca59
Neil Conway <nrc@cs.berkeley.edu>
2013-10-05 16:34:20 -0700

    Tweak for ReaderRetire.
    We can now GC both objects and readers, in part by working around an RSE
    limitation.

--- apps/mv-storage/reader-retire.rb
+++ apps/mv-storage/reader-retire.rb
@@ -5,21 +5,19 @@ class ReaderRetire
   include Bud
 
   state do
-    sealed :object, [:id] => [:val]
-    table :reader, [:id]
+    sealed :obj, [:id] => [:val]
+    sealed :reader, [:id]
     table :reader_done, reader.schema
-    scratch :live_r, reader.schema
     scratch :visible, [:reader, :obj] => [:val]
   end
 
   bloom do
-    live_r <= reader.notin(reader_done)
-    visible <= (live_r * object).pairs {|r,o| r + o}
+    visible <= ((reader * obj).pairs {|r,o| r + o}).notin(reader_done, 0 => :id)
   end
 end
 
 r = ReaderRetire.new
-r.object <+ [[1, "foo"], [2, "bar"]]
+r.obj <+ [[1, "foo"], [2, "bar"]]
 r.reader <+ [[100], [101]]
 r.tick
 puts r.visible.to_a.sort.inspect
@@ -29,8 +27,8 @@ r.tick
 puts r.visible.to_a.sort.inspect
 
 r.reader_done <+ [[101]]
-r.tick
+2.times { r.tick }
 puts r.visible.to_a.sort.inspect
 
-puts "# of objects: #{r.object.to_a.size}"
+puts "# of objects: #{r.obj.to_a.size}"
 puts "# of readers: #{r.reader.to_a.size}"
