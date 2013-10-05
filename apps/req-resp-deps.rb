7e054a9534f8913769a7234b67796690d877f5b6
Neil Conway <nrc@cs.berkeley.edu>
2013-10-04 17:37:16 -0700

    Fixup req-resp + deps.

--- apps/req-resp-deps.rb
+++ apps/req-resp-deps.rb
@@ -28,7 +28,7 @@ class RequestResponseDeps
     resp_chn <~ resp_log
 
     need_resp <= req_log.notin(did_resp, :client => :client, :id => :id)
-    flat_dep <= need_resp {|r| r.deps.map {|d| [r.client, r.id, d]}}
+    flat_dep <= need_resp.flat_map {|r| r.deps.map {|d| [r.client, r.id, d]}}
     missing_dep <= flat_dep.notin(state, :dep => :key)
     do_resp <= need_resp.notin(missing_dep, :client => :client, :id => :id)
     resp_log <= (do_resp * state).pairs(:key => :key) do |r,s|
@@ -48,6 +48,14 @@ class RequestResponseDeps
   end
 end
 
+def do_report(s, c)
+  c.print_resp
+  puts "(Server) Size of req_log: #{s.req_log.to_a.size}"
+  puts "(Server) Size of resp_log: #{s.resp_log.to_a.size}"
+  puts "(Server) Size of did_resp: #{s.did_resp.to_a.size}"
+  puts "(Client) Size of read_req: #{c.read_req.to_a.size}"
+end
+
 opts = { :channel_stats => false, :print_rules => false,
          :disable_rce => false, :disable_rse => false }
 nodes = Array.new(2) { RequestResponseDeps.new(opts) }
@@ -55,17 +63,13 @@ nodes.each(&:tick)
 
 s, c = nodes
 s.state <+ [["foo1", "bar"], ["foo2", "baz"]]
-
 c.read_req <+ [[s.ip_port, c.ip_port, 1, "foo1", ["foo2"]],
                [s.ip_port, c.ip_port, 2, "foo2", ["foo99"]]]
-
 10.times { nodes.each(&:tick); sleep(0.1) }
+do_report(s, c)
 
-c.print_resp
-
-puts "(Server) Size of req_log: #{s.req_log.to_a.size}"
-puts "(Server) Size of resp_log: #{s.resp_log.to_a.size}"
-puts "(Server) Size of did_resp: #{s.did_resp.to_a.size}"
-puts "(Client) Size of read_req: #{c.read_req.to_a.size}"
+s.state <+ [["foo99", "qux"]]
+10.times { nodes.each(&:tick); sleep(0.1) }
+do_report(s, c)
 
 nodes.each(&:stop)
