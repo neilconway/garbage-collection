8d63cc7b298c4f0ba8acc9980a017f37875a63d8
Peter Alvaro <palvaro@gmail.com>
2013-10-11 12:22:53 -0700

    comments

--- apps/mvcc.rb
+++ apps/mvcc.rb
@@ -2,9 +2,13 @@ require 'rubygems'
 require 'bud'
 
 module MVCC
-  # intuition: we can reclaim any versions that are not currently used to
+  # Intuitions: we can reclaim any versions (entries in log())
+  # that are not currently used to:
   # 1) derive a tuple in current(), or
   # 2) derive a tuple in hot_versions()
+  
+  # Further, we can delete inquiry_result() records when their corresponding
+  # log entries appear.
 
   state do
     # both reads and writes to keys are logged at commit time, 
@@ -23,17 +27,19 @@ module MVCC
   bloom do
     flat_dep <= log.flat_map{|l| l.deps.map{|d| [l.id, l.kind, d] unless l.id == d}}
 
-    # updates operate on the most current version.  we assume that updates
+    # Updates operate on the most current written version.  We assume that updates
     # follow 2PL, and check their "version" at commit time -- that is, we
     # consult current() while holding locks.
     # The "current" version for a key is the union of all the read or write
     # action_ids that have referenced it.  The calling process would need to
     # take the union of the two rows returned for reads and writes to this key,
-    # along with any other keys in their read/write set.
+    # along with any other keys in their read/write set (see the test code).
+    # A (read,write) log entry is "current" if no other (read,write) entries
+    # depend on it.
     current <= log.notin(flat_dep, :id => :dep, :kind => :kind)
 
     # a read first does a 'reconnaisance' query to determine the versions
-    # of keys it references.  between this time and when the read completes,
+    # of keys it references.  Between this time and when the read completes,
     # we need to retain the logged version even if it is no longer "current."
     live_reads <= inquiry_result.notin(log, :id => :id)
     hot_versions <= (log * live_reads).lefts(:key => :key, :deps => :dep)
