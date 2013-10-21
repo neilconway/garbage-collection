a4546883ef6881b06bc9d6ec812166f8d75bcb4c
Neil Conway <nrc@cs.berkeley.edu>
2013-10-20 17:09:35 -0700

    Add another test case for causal KVS.

--- apps/test/tc_causal_dict.rb
+++ apps/test/tc_causal_dict.rb
@@ -3,12 +3,18 @@ gem 'minitest'
 require 'minitest/autorun'
 
 class TestCausalDict < MiniTest::Unit::TestCase
-  def test_causal_basic
-    opts = { :quiet => true, :channel_stats => false, :range_stats => false }
+  @@opts = { :quiet => true, :channel_stats => false, :range_stats => false }
+
+  def make_cluster
     ports = (1..3).map {|i| i + 10001}
     addrs = ports.map {|p| "localhost:#{p}"}
-    rlist = ports.map {|p| CausalDict.new(opts.merge(:ip => "localhost", :port => p))}
+    rlist = ports.map {|p| CausalDict.new(@@opts.merge(:ip => "localhost", :port => p))}
     rlist.each {|r| r.node <+ addrs.map {|a| [a]}}
+    rlist
+  end
+
+  def test_causal_basic
+    rlist = make_cluster
 
     # Writes:
     #   (W1) foo -> bar, deps={} (origin = server 1)
@@ -36,7 +42,7 @@ class TestCausalDict < MiniTest::Unit::TestCase
                  [last.id(6), 'qux', 'xxx', [last.id(5)]],
                  [last.id(7), 'baz', 'kkk4', [last.id(6), last.id(5)]]]
 
-    c = CausalDict.new(opts)
+    c = CausalDict.new(@@opts)
     c.read_req <+ [[last.ip_port, c.id(1), 'foo', [first.id(1)]]]
 
     all_nodes = rlist + [c]
@@ -61,6 +67,34 @@ class TestCausalDict < MiniTest::Unit::TestCase
     end
 
     all_nodes.each(&:stop)
+  end
+
+  def test_dominated_gc
+    rlist = make_cluster
+
+    first = rlist.first
+    deps = []
+    10.times do |i|
+      first.log <+ [[first.id(i), "foo", "bar#{i}", deps]]
+      deps = [first.id(i)]
+    end
+
+    15.times { rlist.each(&:tick); sleep 0.1 }
+
+    check_convergence(rlist)
+    rlist.each do |r|
+      assert_equal([].to_set, r.read_buf.to_set)
+      assert_equal([].to_set, r.read_resp.to_set)
+      assert_equal([].to_set, r.log.to_set)
+
+      assert_equal(10, r.safe.length)
+      assert_equal(1, r.safe.physical_size)
+
+      assert_equal(9, r.dominated.length)
+      assert_equal(1, r.dominated.physical_size)
+    end
+
+    rlist.each(&:stop)
   end
 
   def check_convergence(rlist)
