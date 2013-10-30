58e52d49c3df717767b33424e4a9a4c85d81aa19
Emily Andrews <emily@tesba.net>
2013-10-29 19:22:28 -0700

    new partition experiment

--- bench/benchmark.rb
+++ bench/benchmark.rb
@@ -2,7 +2,7 @@
 require "rubygems"
 require "benchmark"
 require "bud"
-require_relative 'causal-dict'
+require_relative '../apps/causal-kvs'
 
 def gen_data(size, percent)
   data = []
@@ -15,7 +15,6 @@ def gen_data(size, percent)
     id += 1
   end
   data = data | gen_dom_data(num_updates, id)
-  #p data
   data
 end
 
@@ -37,86 +36,117 @@ def any_pending?(bud)
 end
 
 def no_partition_bench(data)
-  c = CausalDict.new
+  c = CausalKvsReplica.new
   c.log <+ data
   loop do
     c.tick
     break unless any_pending? c
   end
   raise unless c.log.to_a.empty? and c.dominated.to_a.empty? and c.safe.physical_size == 1
-  #p c.num_tuples
-  c.num_tuples
+  num_tuples(c)
 end
 
-def partition_bench2(size, percent, partition)
+def partition_bench(size)
   rlist = make_cluster
   first = rlist.first
   last = rlist.last
   storage = []
   start = Time.now.to_f
-  data = gen_data(size, percent)
-  data1 = data[0..data.size/2]
+  data = gen_dom_data(size, 0).reverse
   
-  first.log <+ data1
-  (data.size/2 + 5).times {
-    #first.log <+ [data1.pop] 
-    rlist.each(&:tick); 
-    sleep 0.1; 
-    storage << [(start - Time.now.to_f).abs, first.num_tuples] 
-  }
-  data2 = data[data.size/2..-1]
+  disconnect1 = true
+  connect1 = false
+  disconnect2 = true
+  connect2 = false
   
-  disconnect_time = start - Time.now.to_f
-  if partition
-    first.disconnect_channels
-    last.disconnect_channels
-  end
+  disconnect_time = -1
+  connect_time = -1
+  disconnect_time2 = -1
+  connect_time2 = -1
 
-  #first.log <+ data2
-  (data.size/2 + 5).times { 
-    first.log <+ [data2.pop]
-    rlist.each(&:tick); 
+  data.size.times {
+    first.log <+ [data.pop]
+    p data.size
+    4.times { rlist.each(&:tick); }
     sleep 0.1; 
-    storage << [(start - Time.now.to_f).abs, first.num_tuples] 
+    storage << [(start - Time.now.to_f).abs, num_tuples(first)] 
+
+    if (start - Time.now.to_f).abs > 30 and disconnect1 == true
+      disconnect_time = (start - Time.now.to_f).to_f
+      first.disconnect_channels
+      last.disconnect_channels
+      disconnect1 = false
+      connect1 = true
+    end
+
+    if (start - Time.now.to_f).abs > 80 and connect1 == true
+      connect_time = (start - Time.now.to_f).to_f
+      first.connect_channels
+      last.connect_channels
+      connect1 = false
+    end
+
+    if (start - Time.now.to_f).abs > 140 and disconnect2 == true
+      disconnect_time2 = (start - Time.now.to_f).to_f
+      first.disconnect_channels
+      last.disconnect_channels
+      disconnect2 = false
+      connect2 = true
+    end
+
+    if (start - Time.now.to_f).abs > 170 and connect2 == true
+      connect_time2 = (start - Time.now.to_f).to_f
+      first.connect_channels
+      last.connect_channels
+      connect2 = false
+    end
+
+    if (start - Time.now.to_f).abs > 200
+      break
+    end
   }
-
-  connect_time = start - Time.now.to_f
-  if partition
-    first.connect_channels
-    last.connect_channels
-  end
-
-  (data.size/2 + 5).times { rlist.each(&:tick); sleep 0.1; storage << [(start - Time.now.to_f).abs, first.num_tuples] }
-  return storage, disconnect_time, connect_time
+  p storage
+  return storage, disconnect_time, connect_time, disconnect_time2, connect_time2
 end
 
 def make_cluster
   ports = (1..2).map {|i| i + 10001}
   addrs = ports.map {|p| "localhost:#{p}"}
-  rlist = ports.map {|p| CausalDict.new(:ip => "localhost", :port => p)}
+  rlist = ports.map {|p| CausalKvsReplica.new(:ip => "localhost", :port => p)}
   rlist.each {|r| r.node <+ addrs.map {|a| [a]}}
   rlist
 end
 
+def num_tuples(bud)
+  puts "Log: #{bud.log.to_a.size}"
+  puts "safe_log: #{bud.safe_log.to_a.size}"
+  puts "safe: #{bud.safe.physical_size}"
+  sizes = bud.app_tables.map do |t|
+    if t.kind_of? Bud::BudRangeCompress
+      t.physical_size
+    elsif t.kind_of? Bud::BudTable
+      t.to_a.size
+    else
+      0
+    end
+  end
+  sizes.reduce(:+)
+end
+
 def bench(size, percent, variant)
-  data = gen_data(size, percent)
   puts "Run #: size = #{size}, # percent = #{percent}, variant = #{variant}"
 
   case variant
   when "no_partition"
+    data = gen_data(size, percent)
     space_used = no_partition_bench(data)
     $stderr.printf("%d %d %d\n", size, percent, space_used)
   when "partition"
-    storage, disconnect, connect = partition_bench2(size, percent, true)
+    storage, disconnect, connect, disconnect2, connect2 = partition_bench(size)
     $stderr.printf("%f\n", disconnect.abs)
     $stderr.printf("%f\n", connect.abs)
-    storage.each do |s|
-      $stderr.printf("%f %d\n", s[0], s[1])
-    end
-  when "partition_base"
-    storage, disconnect, connect = partition_bench2(size, percent, false)
-    $stderr.printf("%f\n", disconnect.abs)
-    $stderr.printf("%f\n", connect.abs)
+    $stderr.printf("%f\n", disconnect2.abs)
+    $stderr.printf("%f\n", connect2.abs)
     storage.each do |s|
       $stderr.printf("%f %d\n", s[0], s[1])
     end
@@ -128,6 +158,3 @@ end
 raise ArgumentError, "Usage: bench.rb number_updates percent_update variant" unless ARGV.length == 3
 size, percent, variant = ARGV
 bench(size.to_i, percent.to_i, variant)
-
-#partition_bench2(100, 20)
-#bench(100, 20, "partition")
