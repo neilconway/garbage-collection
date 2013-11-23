f03b74de0569bc2f933e945f19308c2c1429090d
Neil Conway <nrc@cs.berkeley.edu>
2013-11-22 20:07:09 -0800

    Checkpoint work on normalized causal KVS.

--- apps/causal-kvs-normal.rb
+++ apps/causal-kvs-normal.rb
@@ -5,6 +5,12 @@ class NormalCausalKvsReplica
   include Bud
 
   state do
+    # Replication
+    sealed :node, [:addr]
+    channel :log_chn, [:@addr, :id] => [:key, :val]
+    channel :dep_chn, [:@addr, :id, :target]
+    channel :log_commit_chn, [:@addr, :id]
+
     # Write operations
     table :log, [:id] => [:key, :val]
     table :safe_log, log.schema
@@ -15,19 +21,33 @@ class NormalCausalKvsReplica
     # Seals for dependency lists
     range :log_commit, [:id]
 
+    # The set of dominated write IDs; we use DR- to reclaim from this.
+    table :dominated, [:id]
+
     scratch :missing_dep, dep.schema
+    scratch :pending_log, log.schema
 
     scratch :same_key, [:w1, :w2]
-    scratch :dominated, [:id]
     scratch :view, log.schema
+  end
+
+  bloom :replication do
+    log_chn <~ (node * log).pairs {|n,l| n + l}
+    dep_chn <~ (node * dep).pairs {|n,d| n + d}
+    log_commit_chn <~ (node * log_commit).pairs {|n,c| n + c}
+
+    log <= log_chn.payloads
+    dep <= dep_chn.payloads
+    log_commit <= log_commit_chn.payloads
   end
 
   bloom :safe do
     # Check write dependencies. We can declare that a write is "safe" when all
     # of the write's dependencies are safe and the dependency list has been
     # sealed.
+    pending_log <= log.notin(safe_log)
     missing_dep <= dep.notin(safe_log, :target => :id)
-    safe_log <+ (log * log_commit).lefts(:id => :id).notin(missing_dep, 0 => :id)
+    safe_log <+ (pending_log * log_commit).lefts(:id => :id).notin(missing_dep, 0 => :id)
   end
 
   bloom :view do
@@ -41,11 +61,13 @@ class NormalCausalKvsReplica
     puts "view: #{view.to_a.inspect}"
     puts "same_key: #{same_key.to_a.inspect}"
     puts "dominated: #{dominated.to_a.inspect}"
+    puts "log: #{log.to_a.inspect}"
+    puts "dep: #{dep.to_a.inspect}"
     puts "=========="
   end
 end
 
-n = NormalCausalKvsReplica.new(:disable_rse => true)
+n = NormalCausalKvsReplica.new(:disable_rse => false)
 n.log <+ [[5, "foo", "bar"]]
 2.times { n.tick }
 n.print_state
