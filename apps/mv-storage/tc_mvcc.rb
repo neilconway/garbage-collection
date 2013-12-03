61e58f86f4e3e41546cb10a37e3ec99bfbffeb85
Peter Alvaro <palvaro@gmail.com>
2013-12-02 21:09:03 -0800

    outer join, and various tick cleanups

--- apps/mv-storage/tc_mvcc.rb
+++ apps/mv-storage/tc_mvcc.rb
@@ -3,71 +3,67 @@ gem 'minitest'
 require 'minitest/autorun'
 require './mvcc_evo'
 
-module BaseData
-  bootstrap do
-    # [:aid] => [:name, :val, :dep]
-    write_log <+ [[1, 1, "foo", "bar", 0],
-            [2, 2, "peter", "thane of glamis", 0],
-            [3, 3, "banquo", "dead but gets kings", 0]]
-
-    # if the analysis has synthesized this auxiliary table, populate it as though
-    # we had used the write() interface properly.
-    write_all_keys <+ [[1],[2],[3]] if self.tables.keys.include? :write_all_keys
+def boots(cls)
+  cls.write <+ [[1, 1, "foo", "bar"],
+            [2, 2, "peter", "thane of glamis"],
+            [3, 3, "banquo", "dead but gets kings"]]
+  if cls.tables.keys.include? :seal_write_batch
+    cls.commit <+ [[1],[2],[3]]
+    cls.seal_write_batch <+ [[1],[2],[3]] 
   end
+  cls.tick; cls.tick
 end
 
 class SimpleWrite
   include Bud
   include AtomicRegister
-  include BaseData
 end
 
 class MultiWrite
   include Bud
   include AtomicBatchWrites
-  include BaseData
 end
 
 class MultiReadWrite
   include Bud
   include AtomicBatchWrites
   include AtomicReads
-  include BaseData
 end
 
-
 class TestMVCCs < Minitest::Test
-  def test_simple
-    s = SimpleWrite.new(:print_rules => true)
-    s.tick;s.tick
-    assert_equal([[1, 1, 'foo', 'bar', 0], [2, 2, 'peter', 'thane of glamis', 0], [3, 3, "banquo", "dead but gets kings", 0]], s.live.to_a.sort)
-    s.write <+ [[100, 100, 'foo', 'baz']]
-    s.tick; s.tick
-    assert_equal([[2, 2, 'peter', 'thane of glamis', 0], [3, 3, "banquo", "dead but gets kings", 0], [100, 100, 'foo', 'baz', 1]], s.live.to_a.sort)
-    4.times{ s.tick }
+  def do_write(s, w)
+    s.write <+ [w]
+    3.times{ s.tick }
+  end
 
+  def do_commit(s, i)
+    s.commit <+ [[i]]
+    s.seal_write_batch <+ [[i]]
+    3.times{ s.tick }
+  end
+
+  def test_simple
+    s = SimpleWrite.new
+    boots(s)
+    assert_equal([[1, 1, 'foo', 'bar', 0], [2, 2, 'peter', 'thane of glamis', 0], [3, 3, "banquo", "dead but gets kings", 0]], s.live.to_a.sort)
+    do_write(s, [100, 100, 'foo', 'baz'])
+    assert_equal([[2, 2, 'peter', 'thane of glamis', 0], [3, 3, "banquo", "dead but gets kings", 0], [100, 100, 'foo', 'baz', 1]], s.live.to_a.sort)
     # confirm that the redundant entries in writes() have been GC'd
     assert_equal([], s.write.to_a)
-
     # confirm that the foo=bar write_log entry has been GC'd
     assert_equal([[2, 2, 'peter', 'thane of glamis', 0], [3, 3, "banquo", "dead but gets kings", 0], [100, 100, 'foo', 'baz', 1]], s.write_log.to_a.sort)
   end
 
   def multi_w_wload(m)
-    m.write <+ [[10, 100,'foo', 'baz']]
-    3.times{ m.tick }
-    m.write <+ [[11, 100, 'peter', 'thane of cawdor']]
-    m.commit <+ [[100]]
-    m.seal_write_batch <+ [[100]]
-    4.times{ m.tick }
+    do_write(m, [10, 100, 'foo', 'baz'])
+    do_write(m, [11, 100, 'peter', 'thane of cawdor'])
+    do_commit(m, 100)
   end 
 
   def multi_wload2(m)
     (1..20).each do |i|
-      m.write <+ [[1000 + i, 'foo', i.to_s]]
-      m.commit <+ [[1000 + i]]
-      m.seal_write_batch <+ [[1000 + i]]
-      4.times{ m.tick }
+      do_write(m, [1000 + i, 'foo', i.to_s])
+      do_commit(m, 1000 + i)
     end
   end
 
@@ -87,11 +83,10 @@ class TestMVCCs < Minitest::Test
     end
   end 
 
-
   def test_multiwrite
-    m = MultiWrite.new(:trace => true, :port => 12345, :print_rules => true)
+    m = MultiWrite.new
+    boots(m)
     setup_multiwrite(m)
-    m.tick; m.tick
     assert_equal([[1, 1, "foo", "bar", 0],[2, 2, "peter", "thane of glamis", 0], [3, 3, "banquo", "dead but gets kings", 0]], m.live.to_a.sort)
     multi_w_wload(m)
     assert_equal([[3, 3, "banquo", "dead but gets kings", 0], [10, 100, "foo", "baz", 1],[11, 100, "peter", "thane of cawdor", 2]], m.live.to_a.sort)
@@ -124,17 +119,20 @@ class TestMVCCs < Minitest::Test
     do_read(m, 200, 'banquo')
     assert_equal([[3, 3, "banquo", "dead but gets kings", 0], [10, 100, "foo", "baz", 1],[11, 100, "peter", "thane of cawdor", 2]], m.live.to_a.sort)
     m.read_commit <+ [[200]]
-    4.times{m.tick}
+    #4.times{m.tick}
+    #2.times{m.tick}
+    m.tick
     assert_equal([], s.read.to_a)
   end
 
   def test_readpath
-    m = MultiReadWrite.new(:print_rules => true, :trace => true, :port => 12346)
+    m = MultiReadWrite.new
+    boots(m)
     multiread_common(m)
     # irrelevant entries GC'd
     assert_equal([[300, 3, 3, "banquo", "dead but gets kings", 0], [300, 10, 100, "foo", "baz", 1], [300, 11, 100, "peter", "thane of cawdor", 2]], m.snapshot.to_a.sort, "irrelevant snapshot entries")
     m.read_commit <+ [[300]]
-    4.times{m.tick}
+    2.times{m.tick}
     assert_equal([], m.snapshot.to_a)
     assert_equal([], m.read.to_a)
     assert_equal([], m.write.to_a)
@@ -147,13 +145,12 @@ class TestMVCCs < Minitest::Test
 
   def test_concurrent_writes
     s = SimpleWrite.new
-    s.tick
+    boots(s)
     s.write <+ [[100, 'foo', 'baz']]
     assert_raises(Bud::KeyConstraintError){ s.write <+ [[100, 'foo', 'qux']] }
     s.write <+ [[101, 'foo', 'qux']]
-    s.live.each{|l| puts "L : #{l}"}
     m = MultiWrite.new
-    m.tick
+    boots(m)
     m.write <+ [[100, 'foo', 'baz']]
     assert_raises(Bud::KeyConstraintError){ m.write <+ [[100, 'foo', 'qux']] }
   end
@@ -169,11 +166,13 @@ class TestMVCCs < Minitest::Test
 
   def test_snapshot_anomaly
     m = MultiWrite.new
-    m.tick
+    boots(m)
+
     results = {}
     results['peter'] = read_name(m, 'peter')
     multi_w_wload(m)
     results['foo'] = read_name(m, 'foo')
+
     assert_equal('thane of glamis', results['peter'].val)
     assert_equal('baz', results['foo'].val)
   end
